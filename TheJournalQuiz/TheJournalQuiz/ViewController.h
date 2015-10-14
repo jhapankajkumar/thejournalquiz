ae7fa3d6894e913472d89ad6295f9d3cb3337f0f
Pankaj Kumar Jha <jha.pankaj55@gmail.com>
2015-10-14 20:26:58 +0530

    Started Initial Project

---
+++ TheJournalQuiz/TheJournalQuiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TheJournalQuiz
+//
+//  Created by Kumar on 14/10/15.
+//  Copyright (c) 2015 Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
